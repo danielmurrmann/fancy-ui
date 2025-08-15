876bc0bb1ff9e189affaca9d6649f731f65a30b9
Daniel Murrmann <9040811+fancyDevelopment@users.noreply.github.com>
2025-08-15 15:27:22 +0200

    Migrated to Angular 20

--- apps/playground/src/app/app.component.ts
+++ apps/playground/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ButtonComponent, IconComponent } from 'fancy-ui-core';
+import { ButtonComponent } from 'fancy-ui-core';
 import { AppBarBundle, AppHeaderBundle, ShellBundle, ViewBundle } from 'fancy-ui-shell';
 import { CardBundle } from 'fancy-ui-content';
 import { FormsBundle } from 'fancy-ui-forms'; 
@@ -8,7 +8,7 @@ import { FormsBundle } from 'fancy-ui-forms';
 @Component({
     selector: 'org-root',
     templateUrl: './app.component.html',
-    imports: [RouterModule, IconComponent, ButtonComponent, ShellBundle, AppHeaderBundle, AppBarBundle, ViewBundle, CardBundle, FormsBundle]
+    imports: [RouterModule, ButtonComponent, ShellBundle, AppHeaderBundle, AppBarBundle, ViewBundle, CardBundle, FormsBundle]
 })
 export class AppComponent {
   title = 'playground';
