ed229601d307f1a27b971270e4b3ebebb256a74d
Daniel Murrmann <9040811+fancyDevelopment@users.noreply.github.com>
2025-11-25 10:35:44 +0100

    Final Versioning for Angular 20

--- packages/fancy-ui-data/src/index.ts
+++ packages/fancy-ui-data/src/index.ts
@@ -2,10 +2,10 @@ import { DatasetCardheaderComponent } from './lib/dataset/dataset-cardheader.com
 import { DatasetItemImageComponent } from './lib/dataset/dataset-item-image.component';
 import { DatasetItemTemplateComponent } from './lib/dataset/dataset-item-template.component';
 import { DatasetItemValueComponent } from './lib/dataset/dataset-item-value.component';
-import { DatasetComponent } from './lib/dataset/dataset.component';
+import { DatasetComponent, SortItem } from './lib/dataset/dataset.component';
 import { PagerComponent } from './lib/pager/pager.component';
 
-export { DatasetComponent, DatasetItemValueComponent, DatasetItemTemplateComponent, DatasetItemImageComponent, DatasetCardheaderComponent }
+export { DatasetComponent, DatasetItemValueComponent, DatasetItemTemplateComponent, DatasetItemImageComponent, DatasetCardheaderComponent, SortItem };
 export const DatasetBundle = [DatasetComponent, DatasetItemValueComponent, DatasetItemTemplateComponent, DatasetItemImageComponent, DatasetCardheaderComponent];
 
 export { PagerComponent };
